cafd12764290796f01762a769dd09c6a1f255322
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-11-12 17:43:14 +0100

    removing junk

--- 3_Signed_Integers/contact.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-contract Contract {
-    uint8 public a = 255;  
-    int8 public b = -128; 
-    int16 public difference = 383;
-}
--- 4_Bytes/contact.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-contract Contract {
-    bytes2 public a;
-    bytes2 public b;
-    bytes2 public cafe = 0xcafe;	
-}
