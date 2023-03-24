f40c1a43c36656d85e42632f16e0c3918e67831f
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-03-24 18:58:30 +0100

    library - is prime

---
+++ libraries/5_Is_Prime/Prime.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+library Prime {
+    function dividesEvenly(uint x, uint y) public pure returns (bool) {
+        return (x % y == 0) ? true : false;
+    } 
+    function isPrime(uint z) public pure returns (bool) {
+        if (z <= 1) {
+            return false;
+        } 
+        for (uint i = 2; i <= z / 2; i++) {
+            if (dividesEvenly(z, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
