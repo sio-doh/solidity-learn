53471e6462773a6c1618cc4274673f916d14469c
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-03-25 20:45:24 +0100

    addresses and accounts - tip owner

---
+++ addresses_and_accounts/4_Tip_Owner/contract.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+    address payable public owner;
+    constructor() {
+        owner = payable(msg.sender);
+    } 
+    event Received(address sender, uint amount);
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    } 
+    function tip() public payable {
+        owner.transfer(msg.value);
+    }
+}
