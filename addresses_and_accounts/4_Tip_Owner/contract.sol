1e7cd6ba3e18b3cbeef20980a82007e9df7a47fe
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-03-27 12:21:00 +0200

    addresses and accounts - tip owner fix

--- addresses_and_accounts/4_Tip_Owner/contract.sol
+++ addresses_and_accounts/4_Tip_Owner/contract.sol
@@ -9,8 +9,9 @@ contract Contract {
     event Received(address sender, uint amount);
     receive() external payable {
         emit Received(msg.sender, msg.value);
-    } 
+    }
     function tip() public payable {
+        require(msg.value > 0, "Tip should be greater than 0"); 
         owner.transfer(msg.value);
     }
 }
