63374c3c1dd6d96fce3d527cb0d0fee5c54cfa82
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-04-18 14:07:00 +0200

    Solidity Challenges - sum and average

---
+++ challenges/1_Sum_and_Average/contract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+contract Contract { 
+
+    function sumAndAverage(uint a, uint b, uint c, uint d) external pure returns (uint, uint) {
+        uint sum = a + b + c + d;
+        uint average = sum / 4;
+        return (sum, average);
+    }
+}
